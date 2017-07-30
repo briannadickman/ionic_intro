905b2821f255991c884c2903b3d40d78b324860a
Bri Dickman <brianna.dickman@gmail.com>
2017-07-29 22:05:48 -0500

    added mock sign in to about page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
+
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public toastCtrl: ToastController) { }
 
+   confirmSignIn(position: string) {
+    let confirm = this.toastCtrl.create({
+      message: 'You have clicked the Sign In button!',
+      duration: 3000
+    });
+
+    confirm.present(confirm);
   }
 
 }
